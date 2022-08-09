79592843961aa576b9d1812d5fb4b1a9654dfceb
Shane <navysupra@gmail.com>
2022-08-09 17:34:12 +0000

    getting stuff to work

--- server.js
+++ server.js
@@ -3,20 +3,33 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+
 /* Tcp:Http */
 const PORT = 8080;
 
 /* Middleware */
+app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 
 /* Arguments & Properties */
+const starShips = {
+  '1701': { question: "Intercepted V'ger", answer: "USS ENTERPRISE" },
+  '2000': { question: "The great experiment", answer: "USS EXCELSIOR" },
+  '1864': { question: "Commandeered by Khan", answer: "USS RELIANT" }
+};
 /* Classes */
 /* Export Functions */
 /* Local Functions */
 /* Endpoints */
-app.get('/', (rx, tx) => {
 
+/* Browse */
+app.get('browse', (request, response) => {
+  response.render('browse', { starShips });
 });
+/* Read */
+/* Edit */
+/* Add */
+/* Delete */
 
 /* Execution & Test Data */
 app.listen(PORT, () => {
@@ -24,3 +37,6 @@ app.listen(PORT, () => {
 });
 
 /* Exports */
+
+
+
